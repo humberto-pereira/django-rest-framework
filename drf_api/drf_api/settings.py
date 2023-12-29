a232e440645391eec7fb2537d2f6e109bd936cb6
Humberto Pereira <humberto_pereira@live.com>
2023-12-29 23:59:14 +0100

    commit

--- drf_api/drf_api/settings.py
+++ drf_api/drf_api/settings.py
@@ -64,7 +64,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'd-r-framework.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'd-r-framework-03171bbf61e6.herokuapp.com/']
 
 
 # Application definition
