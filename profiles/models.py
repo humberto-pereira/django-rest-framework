277dc98a6665cba53d40a30e255d5c92308bbb5c
Humberto Pereira <humberto_pereira@live.com>
2024-01-08 21:34:30 +0100

    commit

--- profiles/models.py
+++ profiles/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_profile_fgimrx'
+        upload_to='images/', default='django-rest-framework/default_profile_vug5i3'
     )
 
     class Meta:
