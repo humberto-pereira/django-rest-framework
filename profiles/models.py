e4db107f90dd8db17ac5606e39ab8e26b325ed76
Humberto Pereira <humberto_pereira@live.com>
2024-01-10 17:48:14 +0100

    commit

--- profiles/models.py
+++ profiles/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='https://res.cloudinary.com/h-pereira/image/upload/v1/media/images/default_profile_qxupvm'
+        upload_to='images/', default='../default_profile_qxupvm'
     )
 
     class Meta:
